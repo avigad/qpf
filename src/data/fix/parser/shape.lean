1379b211883cb92d59c21d306dc5e310bca4e135
Simon Hudon <simon.hudon@gmail.com>
2019-03-15 02:35:45 -0400

    fix corecursor for cofixpoint of multivariate functors

--- src/data/fix/parser/shape.lean
+++ src/data/fix/parser/shape.lean
@@ -13,28 +13,32 @@ meta def foo (n : expr) (v : expr) : type_cnstr → type_cnstr
   args := args.map $ λ e, expr.replace e $ λ e' _, if e' = n then pure v else none,
   result := result.map $ λ e, expr.replace e $ λ e' _, if e' = n then pure v else none }
 
-open expr
-meta def mk_shape_decl (d : inductive_type) : tactic inductive_type :=
+open expr interactive
+
+meta structure datatype_shape extends internal_mvfunctor :=
+(hole : expr)
+
+meta def mk_shape_decl (d : inductive_type) : tactic (expr × inductive_type) :=
 do v ← mk_local_def (fresh_univ (d.params.map expr.local_pp_name) `α) d.type,
    let c := @const tt d.name d.u_params,
    let c' := c.mk_app d.params,
-   pure { pre := d.name.get_prefix,
-          name := d.name <.> "shape",
-          u_names := d.u_names,
-          params := d.params ++ [v],
-          idx := d.idx,
-          type := d.type,
-          ctors := d.ctors.map (foo c' v) }
+   pure (v, { pre := d.name.get_prefix,
+              name := d.name <.> "shape",
+              u_names := d.u_names,
+              params := d.params ++ [v],
+              idx := d.idx,
+              type := d.type,
+              ctors := d.ctors.map (foo c' v) })
 
-meta def mk_shape_functor' (d : interactive.inductive_decl) : lean.parser (internal_mvfunctor × internal_mvfunctor) :=
+meta def mk_shape_functor' (d : interactive.inductive_decl) : lean.parser (datatype_shape × internal_mvfunctor) :=
 do d ← inductive_type.of_decl d,
    func' ← internalize_mvfunctor d,
-   func ← mk_shape_decl d,
+   (v,func) ← mk_shape_decl d,
    mk_inductive' func,
    func ← internalize_mvfunctor func,
    mk_internal_functor_def func,
    mk_internal_functor_eqn func,
-   pure (func, func')
+   pure ({ hole := v .. func }, func')
 
 meta def decl_kind : declaration → string
 | (declaration.defn a a_1 a_2 a_3 a_4 a_5) := "defn"
@@ -44,7 +48,7 @@ meta def decl_kind : declaration → string
 
 
 meta def mk_fixpoint (fix : name) (func d : internal_mvfunctor) : tactic unit :=
-do  let dead := func.dead_params.map prod.fst,
+do let dead := func.dead_params.map prod.fst,
    -- let n := dead.length,
    let shape := (@const tt (func.decl.to_name <.> "internal") func.induct.u_params).mk_app dead,
    -- trace "shape", trace n,
@@ -59,17 +63,19 @@ do  let dead := func.dead_params.map prod.fst,
    df ← lambdas d.params $ (@const tt intl_n func.induct.u_params).mk_app (d.dead_params.map prod.fst ++ [v]),
    t  ← infer_type df,
    -- trace "- fixpoint", trace func.decl.to_name.get_prefix, trace df, trace t,
-   add_decl $ mk_definition func.decl.to_name.get_prefix func.induct.u_names t df
+   add_decl $ mk_definition func.decl.to_name.get_prefix func.induct.u_names t df,
+   pure ()
 
-meta def mk_datatype (iter : name) (d : inductive_decl) : parser internal_mvfunctor :=
-do (func, d) ← mk_shape_functor' d,
+meta def mk_datatype (iter : name) (d : inductive_decl) : parser (datatype_shape × internal_mvfunctor) :=
+do (func', d) ← mk_shape_functor' d,
+   let func : internal_mvfunctor := { .. func' },
    mk_mvfunctor_instance func,
    mk_pfunctor func,
    mk_pfunc_constr func,
    mk_pfunc_recursor func,
    mk_mvqpf_instance func,
    mk_fixpoint iter func d,
-   pure d
+   pure (func',d)
 
 end tactic
 
